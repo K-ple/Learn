42a7400ca082a3dc6b6f68c17cbb726355778a16
Kple <alex0802@naver.com>
2023-08-13 20:08:38 +0900

    2023-08-13

---
+++ Back_End_Code/Java/first_java.java
@@ -0,0 +1,7 @@
+package Back_End_Code.Java;
+
+public class first_java {
+    public static void main(String[] args) {
+        System.out.println("Hello java");    
+    }    
+}
---
+++ Fornt_End_Code/HTML_CSS_JS/max_number.js
@@ -0,0 +1,5 @@
+function maxnumber(arr){
+    return Math.max(...arr)
+}
+const max = maxnumber([10,50,30]);
+console.log(max)
