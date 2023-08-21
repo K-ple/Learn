4150d9c0c6b9bb7e33a7bc2ae97260cb04f3e892
Kple <alex0802@naver.com>
2023-08-21 18:15:12 +0900

    2023-08-21

--- Back_End_Code/Coding_Data_Structure/시각.py
+++ Back_End_Code/Coding_Data_Structure/시각.py
@@ -1,1 +1,13 @@
-aa
+n = int(input())
+answer = 0
+for i in range(n+1):
+    if i == 3:
+        answer+= 3600
+        continue
+    for j in range(60):
+        if '3' in str(j):
+            answer += 60
+        else:
+            answer += 15
+
+print(answer)
