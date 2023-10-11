ff4b8cdb4d090bea924988a7f2bb4c2d42e4da4b
Kim TaeUk <alex0802@naver.com>
2023-10-11 17:04:34 +0900

    2023-10-11

--- Front_End_Code/Vue.js/vue_todoit/src/components/TodoInput.vue
+++ Front_End_Code/Vue.js/vue_todoit/src/components/TodoInput.vue
@@ -18,7 +18,7 @@
             addTodo() {
                 if (this.newTodoItem !== ""){
                     var value = this.newTodoItem && this.newTodoItem.trim();
-                    localStorage.setItem(value, value);
+                    this.$emit('addTodo',value);
                     this.clearInput();
                 }
             },
