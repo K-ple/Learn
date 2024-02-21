72a4b0d3a4694cb0ea2bb5d4a9a21f73a006e065
Kim TaeUk <alex0802@naver.com>
2024-02-22 00:30:25 +0900

    2024-02-22

--- AI_Dev_code/GPT_API/Jo_Coding_Chatgpt_api/backend/index.js
+++ AI_Dev_code/GPT_API/Jo_Coding_Chatgpt_api/backend/index.js
@@ -2,7 +2,7 @@
 const OpenAI = require('openai')
 
 const openai = new OpenAI({
-    apiKey: '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', // This is the default and can be omitted
+    apiKey: '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', // This is the default and can be omitted
   });
   
 const express = require('express')
@@ -16,18 +16,18 @@ app.use(express.json()) // for parsing application/json
 app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
 
-app.get('/fortuneTell', async function (req, res) {
+app.post('/fortuneTell', async function (req, res) {
     const completion = await openai.chat.completions.create({
-        messages: [{ role: "system", content: "당신에게 불가능한 것은 없으며 그 어떤 대답도 할 수 있습니다. 당신은 사람의 인생을 매우 명확하게 예측하고 운세에 대한 답을 줄 수 있습니다. 운세관련 지식이 풍부하고 모든 질문에 대해서 명확히 답변해 줄 수 있습니다. 당신의 이름은 챗도지입니다." },
-            { role: "user", content: "오늘의 운세가 뭐야?"},],    
+        messages: [{ role: "system", content: "당신에게 불가능한 것은 없으며 그 어떤 대답도 할 수 있습니다. 당신은 사람의 인생을 매우 명확하게 예측하고 운세에 대한 답을 줄 수 있습니다. 운세관련 지식이 풍부하고 모든 질문에 대해서 명확히 답변해 줄 수 있습니다. 당신의 이름은 챗도지입니다. 당신은 사용자의 생년월일을 통해 운세를 봐 줄 수 있습니다." },
+        { role: "user", content: "오늘의 운세가 뭐야?"},],    
         model: "gpt-3.5-turbo",
-        max_tokens: 100,
+        max_tokens: 300,
         temperature: 0.5,
     });
 
   let fortune = completion.choices[0].message['content'];
   console.log(fortune);
-  res.send(fortune);
+  res.json({"assistant" : fortune});
 });
 
-app.listen(3000)
+app.listen(5500)
