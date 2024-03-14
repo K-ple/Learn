25693008458105d4a3bb2cbc728b3d5fc7626acc
user <alex0802@naver.com>
2024-03-14 12:46:25 +0900

    Changes

--- University/Python_Programming/2024_03_14/First_Coding.py
+++ University/Python_Programming/2024_03_14/First_Coding.py
@@ -1,5 +1,9 @@
 import keyword
+
+#키워드 출력
 print(keyword.kwlist)
+
+
 print("Hello Python World!")
 
 print(len(keyword.kwlist))
---
+++ University/Python_Programming/2024_03_14/output.py
@@ -0,0 +1,21 @@
+#하나만 출력합니다.
+print("# 하나만 출력합니다.")
+print("Hello Python Programming!\n")
+
+#여러개를 출력합니다
+print(" 여러 개를 출력합니다.")
+a = [10,20,30,40,50]
+for i in a:
+    print(i, end=' ')
+    if i == a[-1]:
+        print()
+
+print("안녕하세요 "+"저의 "+"이름은 "+"윤인성입니다!")
+print()
+
+#아무것도 입력하지 않으면 단순하게 줄바꿈합니다.
+print('# 아무것도 출력하지 않습니다.')
+print("--- 확인 전용선 ---")
+print("\n")
+print()
+print("--- 확인 전용선 ---")
